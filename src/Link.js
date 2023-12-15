19d0a02a04e018ad98e83612b0465b8e94f756e6
Roland Krisztandl <roli12140@gmail.com>
2023-12-15 23:49:34 +0100

    removed public url form the images

--- src/Link.js
+++ src/Link.js
@@ -7,7 +7,7 @@ export default function Link(props) {
   return (
     <div className={"Link"}>
       <a href={link.url}>
-        <img src={process.env.PUBLIC_URL + link.icon} alt={link.title} />
+        <img src={link.icon} alt={link.title} />
         <p>{link.title}</p>
       </a>
     </div>
