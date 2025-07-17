a53795f11af887540a0d1039067c1d128ead9673
krr0land <roli12140@gmail.com>
2025-07-17 21:21:13 +0200

    fix: missing table border

--- src/Components/TechConfig.tsx
+++ src/Components/TechConfig.tsx
@@ -7,28 +7,31 @@ export default function TechConfig() {
       <h1 className="md:pt-20 text-2xl font-bold">Tech Config</h1>
 
       <table className="border-collapse m-2 border-transparent border-8 rounded-lg backdrop-blur-sm">
-        {techConfig.map((group) => (
-          <>
-            <tr className="text-xl font-bold h-16" key={group.title}>
-              <td colSpan={3}>{group.title}</td>
-            </tr>
-            {group.entries.map((entry) => (
-              <tr className="border-y-[1px]" key={entry.product}>
-                <td className="p-2">{entry.category}</td>
-                <td className="p-2">
-                  {entry.url ? (
-                    <a href={entry.url} target="_blank" rel="noopener noreferrer">
-                      <LinkIcon className="fill-gray-200/80" />
-                    </a>
-                  ) : (
-                    <div />
-                  )}
-                </td>
-                <td className="p-2">{entry.product}</td>
+        <tbody>
+          {techConfig.map((group) => (
+            <>
+              <tr className="text-xl font-bold h-16" key={group.title}>
+                <td colSpan={3}>{group.title}</td>
               </tr>
-            ))}
-          </>
-        ))}
+              {group.entries.map((entry) => (
+                <tr className="border-y-[1px]" key={entry.product}>
+                  <td className="p-2">{entry.category}</td>
+                  <td className="p-2">
+                    {entry.url ? (
+                      <a href={entry.url} target="_blank" rel="noopener noreferrer">
+                        <LinkIcon className="fill-gray-200/80" />
+                      </a>
+                    ) : (
+                      <div />
+                    )}
+                  </td>
+                  <td className="p-2">{entry.product}</td>
+                </tr>
+              ))}
+            </>
+          ))}
+          <tr className="h-2" />
+        </tbody>
       </table>
     </>
   );
