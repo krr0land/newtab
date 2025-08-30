3127e1c57bbcbff1defaf36e68149baabe5aace3
Roland Krisztandl <roli12140@gmail.com>
2025-08-30 16:13:45 +0200

    change bg

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
   const currentLocation = useLocation().pathname;
   return (
     <>
-      <div className="bg-gray-400/30 dark:bg-gray-600/30 md:absolute top-2 left-2 py-1 px-1 rounded-xl backdrop-blur-xs">
+      <div className="md:absolute top-2 left-2 py-1 px-1 ">
         <div className="flex items-center gap-2">
           <NavBarLink to={routes.HOME} icon={HomeIcon} current={currentLocation} />
           <NavBarLink to={routes.WEATHER} icon={WeatherIcon} current={currentLocation} />
@@ -23,7 +23,7 @@ function NavBarLink(props: { to: string; icon: React.ComponentType<{ className?:
   const isActive = props.current === props.to;
   return (
     <Link to={props.to}>
-      <props.icon className={`${isActive ? "fill-gray-800/90 dark:fill-gray-200/80" : "fill-gray-500/80 dark:fill-gray-600/50"} w-9 h-9 p-[2px] border-2 border-transparent hover:bg-gray-600/22 rounded-lg`} />
+      <props.icon className={`${isActive ? "bg-gray-400/80 dark:bg-gray-800/80" : "hover:bg-gray-300/80 dark:hover:bg-gray-800/60"} fill-zinc-700 dark:fill-white w-9 h-9 p-[2px] rounded-lg`} />
     </Link>
   );
 }
